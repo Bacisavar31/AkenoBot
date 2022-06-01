80d23ccf0b82685185351ae6683ca8ee4f32c8fa
Bacisavar31 <sirdogesirdoge@gmail.com>
2022-06-01 23:16:25 +0300

    avatar2

--- commands/action/avatar.js
+++ commands/action/avatar.js
@@ -6,7 +6,7 @@ module.exports = {
     permissions: [],
     devOnly: false,
     run: async ({client, message, args}) => {
-        let user = message.mentions.user.first() || client.users.cache.get(args[0]) || message.author;
+        let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
 
         let embed = new MessageEmbed()
         .setTitle(`${user.username}'s avatar`)
